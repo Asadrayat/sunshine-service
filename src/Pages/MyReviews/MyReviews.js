b2294d1ef3d804a23a59911c1e9b2972c1bfb6bb
Asadrayat <asad.rayat4@gmail.com>
2022-11-10 03:07:05 +0600

    added my review

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,13 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Context/Authprovider/Authprovider';
+import MyReviewsDetails from '../ReviewrRow/MyReviewsDetails/MyReviewsDetails';
 
 
 const MyReviews = () => {
-    const {user} = useContext(AuthContext);
-    console.log(user);
+    const { user } = useContext(AuthContext);
+    const [myreviews, setMyReviews] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:5000/reviews?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                setMyReviews(data);
+                console.log(data);
+            })
+    }, [user?.email])
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure, you want to cancel this order');
+        if (proceed) {
+            fetch(`http://localhost:5000/reviews?email=${id}`, {
+                method: 'DELETE',
+
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully');
+                        const remaining = myreviews.filter(rvw => console.log(rvw));
+                        setMyReviews(remaining);
+                    }
+                })
+        }
+    }
     return (
         <div>
-            
+            {
+                myreviews.map(myreview => <MyReviewsDetails
+                    key={myreview._id}
+                    myreview={myreview}
+                    handleDelete={handleDelete}
+                ></MyReviewsDetails>)
+            }
         </div>
     );
 };
---
+++ src/Pages/ReviewrRow/MyReviewsDetails/MyReviewsDetails.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const MyReviewsDetails = ({ myreview ,handleDelete }) => {
+    
+    return (
+        <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+                {/* <!-- head --> */}
+                <thead>
+
+                    <tr>
+                        <th>Delete</th>
+                        <th>Name</th>
+                        <th>Feedback</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+
+                    }
+                </tbody>
+                <tr>
+                    <th>
+                        <label>
+                            <button onClick={() => handleDelete(_id)} className='btn btn-ghost'>X</button>
+                        </label>
+                    </th>
+                    <td>
+                        <div className="flex items-center space-x-3">
+                            <div className="avatar">
+                                <div className="mask mask-squircle w-12 h-12">
+                                    <img src={myreview?.photo} alt="Avatar Tailwind CSS Component" />
+                                </div>
+                            </div>
+                            <div>
+                                <div className="font-bold">{myreview?.customer}</div>
+                                <div className="text-sm opacity-50">{myreview?.email}</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <span className="badge badge-ghost badge-sm">{myreview?.message}</span>
+                    </td>
+
+                </tr>
+
+
+            </table>
+        </div>
+
+    );
+};
+
+export default MyReviewsDetails;
